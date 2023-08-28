4bf76e0939fc8fe5ac07bac4f6b3a760fa5e415c
Serle Shuman <serle.shuman@gmail.com>
2023-08-28 13:25:39 +0100

    stage1-s1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod models;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Welcome To My-Jira!");
 }
---
+++ src/models.rs
@@ -0,0 +1,41 @@
+#![allow(dead_code)]
+
+use std::collections::HashMap;
+
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed, 
+}
+
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+    pub stories: Vec<u32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+       Self { name, description, status: Status::Open, stories: vec![] }
+    }
+}
+
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        Self { name, description, status: Status::Open }
+    }
+}
+
+pub struct DBState {
+    last_item_id: u32, 
+    epics: HashMap<u32, Epic>, 
+    stories: HashMap<u32, Story>,
+}
